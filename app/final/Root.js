b99542aa0340e59bcedf37973f3d39bce7a9dffa
chenwenhua@juqitech.com <chenwenhua@juqitech.com>
2017-10-11 11:41:55 +0800

    配置webpack

---
+++ app/final/Root.js
@@ -0,0 +1,12 @@
+import React, { render } from 'react';
+let Root = React.createClass({	
+    render() {
+        return (
+            <div>
+            	hello, react! hhaha
+            </div>
+        );
+    }
+});
+
+export default Root;
